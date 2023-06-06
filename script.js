bc824507b304c90ea538d41275b2fec34172a576
Md:Meheraj <94986248+Meheraj2003@users.noreply.github.com>
2023-06-06 14:24:22 +0600

    First-upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// To top navvigation items
+let toTop=document.querySelector('.to-top');
+window.addEventListener('scroll',()=>{
+       if(window.pageYOffset>80){
+              toTop.classList.add('active')
+       }
+       else{
+              toTop.classList.remove('active')
+       }
+})
